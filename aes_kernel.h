a622952176439fab0574bf4aead4b8e7651103f1
root <root@localhost.localdomain>
2015-02-14 15:38:27 -0500

    Added aes hash functions

--- aes_kernel.h
+++ aes_kernel.h
@@ -24,6 +24,16 @@ void AES_cbc_128_encrypt_gpu(const uint8_t *in_d,
 			     const unsigned int threads_per_blk,
 			     cudaStream_t stream = 0);
 
+void AES_cbc_128_hash_gpu(const uint8_t *in_d,
+			     uint8_t *out_d,
+			     const uint32_t* pkt_offset_d,
+			     const uint8_t *keys_d,
+			     uint8_t *ivs_d,
+			     const unsigned int numFlows,
+			     uint8_t *checkbits_d,
+			     const unsigned int threads_per_blk,
+			     cudaStream_t stream = 0);
+
 void AES_ecb_128_encrypt_gpu(const uint8_t *in_d,
 			     uint8_t * out_d,
 			     const uint8_t * keys_d,
